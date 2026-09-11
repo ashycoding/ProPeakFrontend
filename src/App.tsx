11228963460f70c9371f0f11b0d4b53ca9adb05d
Ashy Coding <ashycoding@example.com>
2026-09-11 11:47:04 +0530

    Clean up temporary test endpoints and diagnostics page

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ import StartProjectPage from '@/pages/StartProjectPage'
 import PrivacyPage from '@/pages/PrivacyPage'
 import TermsPage from '@/pages/TermsPage'
 import NotFoundPage from '@/pages/NotFoundPage'
-import TestStatusPage from '@/pages/TestStatusPage'
 
 export const App: React.FC = () => {
   return (
@@ -36,9 +35,6 @@ export const App: React.FC = () => {
             <Route path="/start-project" element={<StartProjectPage />} />
             <Route path="/privacy" element={<PrivacyPage />} />
             <Route path="/terms" element={<TermsPage />} />
-            <Route path="/test" element={<TestStatusPage />} />
-            <Route path="/test-status" element={<TestStatusPage />} />
-            <Route path="/diagnostics" element={<TestStatusPage />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </main>
